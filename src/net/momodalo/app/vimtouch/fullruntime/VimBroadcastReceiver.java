11af813665b4d75b7565267564840bc30758a462
David Tao <momodalo@gmail.com>
2013-01-01 22:59:52 +0800

    initial

---
+++ src/net/momodalo/app/vimtouch/fullruntime/VimBroadcastReceiver.java
@@ -0,0 +1,14 @@
+package net.momodalo.app.vimtouch.fullruntime;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class VimBroadcastReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context arg0, Intent arg1) {
+
+    }
+
+}
